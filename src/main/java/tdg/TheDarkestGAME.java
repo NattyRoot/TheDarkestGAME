38f04cbeb1f50008b77fc940558cc33f52a2eca2
Natty Root <felix.soignon@gmail.com>
2023-07-09 21:31:47 +0200

    Initial commit w/ game panel

---
+++ src/main/java/tdg/TheDarkestGAME.java
@@ -0,0 +1,26 @@
+package tdg;
+
+
+import tdg.ui.GamePanel;
+
+import javax.swing.*;
+
+public class TheDarkestGAME {
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("The Darkest GAME");
+
+        GamePanel gamePanel = new GamePanel();
+
+        window.add(gamePanel);
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+
+        window.setVisible(true);
+    }
+}
